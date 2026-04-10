19878c853eb09a40478573642e374dcd323137a5
Nick Maiorov <nickwave@protonmail.com>
2026-04-10 16:11:24 +0300

    Changes

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,15 @@
 export default class Api {
-    static baseURL = import.meta.env.VITE_API_BASE_URL;
+  // static baseURL = import.meta.env.VITE_API_BASE_URL;
 
-	static async loadLatestResults() {
-		try {
-			const response = await fetch(`${Api.baseURL}/api/results/edf626f95530e3929b41cf96fe3d9e2b`);
-			const data = await response.json();
-			return data;
-		} catch (e) {
-			console.log(e);
-		}
-	}
+  static async loadLatestResults() {
+    try {
+      const response = await fetch('data/results_data.json');
+      const data = await response.json();
+      // const response = await fetch(`${Api.baseURL}/api/results/edf626f95530e3929b41cf96fe3d9e2b`);
+      // const data = await response.json();
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
