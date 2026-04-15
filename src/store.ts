5b7dc4d6615b33bd8f0ce9c5bc8cf491a577f607
Nick Maiorov <nickwave@protonmail.com>
2026-04-15 19:21:02 +0300

    Changes

--- src/store.ts
+++ src/store.ts
@@ -199,7 +199,7 @@ function recalculateTurnoutPercents() {
   // const lastGlobalTurnout = Object.values(turnouts2026.value.global_turnouts).pop();
   // turnoutPercents.value = lastGlobalTurnout;
 
-  turnoutPercents.value = 79.55;
+  turnoutPercents.value = 79.56;
 }
 
 function recalculatePolygonColors() {
